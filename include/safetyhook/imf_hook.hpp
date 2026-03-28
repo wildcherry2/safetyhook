6c4d7d36e5939a3dc2f64229d025e6876f499328
wildcherry2 <tylercwray@gmail.com>
2026-03-28 17:17:52 -0400

    Update dependencies. Continue prototype implementation. Remove ptmf_helper.hpp.

--- include/safetyhook/imf_hook.hpp
+++ include/safetyhook/imf_hook.hpp
@@ -1,7 +1,7 @@
 #pragma once
-#include "BindingThunk.hpp"
+#include "BindingThunk/BindingThunk.hpp"
+#include "BindingThunk/RestoreThunk.hpp"
 #include "safetyhook/inline_hook.hpp"
-#include "safetyhook/ptmf_helper.hpp"
 
 #include <variant>
 
@@ -10,48 +10,97 @@ namespace safetyhook {
  * @note The instance must outlive the hook.
  * @attention Detouring functions that return by value but are implicitly references at the ABI level will not work.
  */
-class SAFETYHOOK_API InlineToMemberFunctionHook {
+class SAFETYHOOK_API InlineToMemberFunctionHook final {
 public:
-    using IMFHError = std::variant<InlineHook::Error, BindingThunk::FThunkError>;
+    enum Flags : int {
+        Default = 0,            ///< Default flags.
+        StartDisabled = 1 << 0, ///< Start the hook disabled.
+        SaveContext = 1 << 1, ///< Save and restore non-argument register context before calling the original function.
+        PackArgs = 1 << 2,    ///< Pack arguments into an ArgumentContext& during binding.
+    };
 
-    /// @brief Create an inline hook whose destination is a member function with a specific instance.
-    /// @param target The address of the function to hook.
-    /// @param instance The instance to bind to; destination must be a member of instance's type.
-    /// @param flags The flags to use.
-    /// @tparam Destination The pointer to the member function to detour to.
-    /// @return The InlineToMemberFunctionHook or an InlineToMemberFunctionHook::Error if an error occurred.
-    /// @note This will use the default global Allocator.
-    /// @attention Detouring functions that return by value but are implicitly references at the ABI level will not work.
-    template <typename Target, auto Destination>
-        requires PointerToMemberFunction<Destination>
-    [[nodiscard]] static std::expected<InlineToMemberFunctionHook, IMFHError> create(Target target,
-        PTMFHelper<decltype(Destination)>::ClassType* instance, InlineHook::Flags flags = InlineHook::Flags::Default) {
-        using Helper = PTMFHelper<decltype(Destination)>;
-        const auto signature = Helper::GetSignature();
-        if (!signature)
-            return std::unexpected<IMFHError>(signature.error());
-        return create(reinterpret_cast<void*>(target), reinterpret_cast<void*>(instance),
-            reinterpret_cast<void*>(&Helper::template StaticInvoker<Destination>), signature, flags);
+    enum CreateError : int { IncorrectUsageOfArgumentContext = 0 };
+
+    using IMFHError = std::variant<InlineHook::Error, BindingThunk::FThunkError, CreateError>;
+
+    template <auto Destination, Flags Flag, typename TargetReturnType, typename... TargetArgs>
+        requires(BindingThunk::MemberFunctionValue<Destination> && static_cast<bool>(Flag & PackArgs))
+    static std::expected<InlineToMemberFunctionHook, IMFHError> create(
+        BindingThunk::MemberFunctionHelper<decltype(Destination)>::ClassType* instance,
+        TargetReturnType (*target)(TargetArgs...)) {
+        using Helper = BindingThunk::MemberFunctionHelper<decltype(Destination)>;
+        static_assert(Helper::IsArgumentContextCallback,
+            "To use the PackArgs flag, you must have a valid ArgumentContext-signature member function "
+            "(void(Class::*)(ArgumentContext&))!");
+        constexpr BindingThunk::EBindingThunkType thunk_flags =
+            BindingThunk::EBindingThunkType::Argument |
+            (Flag & SaveContext ? BindingThunk::EBindingThunkType::Register
+                                : static_cast<BindingThunk::EBindingThunkType>(0));
+        BindingThunk::FThunkResult bind_result =
+            BindingThunk::GenerateBindingThunk<Destination, thunk_flags, TargetReturnType, TargetArgs...>(instance);
+        if (!bind_result)
+            return std::unexpected(bind_result.error());
+
+        InlineToMemberFunctionHook hook{};
+
+        hook.m_binder = std::move(*bind_result);
+        auto hook_result = InlineHook::create(target, hook.m_binder.get(), InlineHook::StartDisabled);
+        if (!hook_result) return std::unexpected(hook_result.error());
+        hook.m_hook = std::move(*hook_result);
+
+        typename Helper::FreeFunctionType real_target = hook.m_hook.original<typename Helper::FreeFunctionType>();
+        BindingThunk::FThunkResult restore_result = BindingThunk::GenerateRestoreThunk(real_target, thunk_flags);
+        if (!restore_result)
+            return std::unexpected(bind_result.error());
+        hook.m_restorer = std::move(*restore_result);
+
+        if constexpr(Flag & StartDisabled) return hook;
+        if (auto enable_result = hook.enable(); !enable_result)
+            return std::unexpected(enable_result.error());
+
+        return hook;
     }
 
-    /**
-     * @brief Create an inline hook whose destination is a member function with a specific instance.
-     * @param target The address of the function to hook.
-     * @param instance The instance to bind to.
-     * @param static_invoker The static invoker of the member function. If not autogenerated from the templated version
-     * of @ref create, it must match the static signature of the member function (`this` pointer as first parameter,
-     * with arguments as remaining parameters) and call the member function with the given `this` and arguments.
-     * @param signature The built ABISignature to generate the thunk with.
-     * @param flags The flags to use.
-     * @return The InlineToMemberFunctionHook or an InlineToMemberFunctionHook::Error if an error occurred.
-     * @note This will use the default global Allocator.
-     * @attention Detouring functions that return by value but are implicitly references at the ABI level will not work.
-     */
-    [[nodiscard]] static std::expected<InlineToMemberFunctionHook, IMFHError> create(void* target, void* instance,
-        void* static_invoker, const BindingThunk::ABISignature& signature,
-        InlineHook::Flags flags = InlineHook::Flags::Default);
+    template <auto Destination, Flags Flag>
+        requires(BindingThunk::MemberFunctionValue<Destination> && !static_cast<bool>(Flag & PackArgs))
+    static std::expected<InlineToMemberFunctionHook, IMFHError> create(
+        typename BindingThunk::MemberFunctionHelper<decltype(Destination)>::ClassType* instance,
+        typename BindingThunk::MemberFunctionHelper<decltype(Destination)>::FreeFunctionType target) {
+        using Helper = BindingThunk::MemberFunctionHelper<decltype(Destination)>;
+        static_assert(
+            !Helper::ContainsArgumentContext, "ArgumentContext is not allowed without the PackArgs flag set!");
 
-    InlineToMemberFunctionHook() = default;
+        constexpr BindingThunk::EBindingThunkType thunk_flags =
+            Flag & SaveContext ? BindingThunk::EBindingThunkType::Register : BindingThunk::EBindingThunkType::Default;
+        BindingThunk::FThunkResult bind_result = BindingThunk::GenerateBindingThunk<Destination, thunk_flags>(instance);
+        if (!bind_result)
+            return std::unexpected(bind_result.error());
+
+        InlineToMemberFunctionHook hook{};
+
+        hook.m_binder = std::move(*bind_result);
+        auto hook_result = InlineHook::create(target, hook.m_binder.get(), InlineHook::StartDisabled);
+        if (!hook_result) return std::unexpected(hook_result.error());
+        hook.m_hook = std::move(*hook_result);
+
+        if constexpr (thunk_flags == BindingThunk::EBindingThunkType::Register) {
+            typename Helper::FreeFunctionType real_target = hook.m_hook.original<typename Helper::FreeFunctionType>();
+            BindingThunk::FThunkResult restore_result = BindingThunk::GenerateRestoreThunk(real_target, thunk_flags);
+            if (!restore_result)
+                return std::unexpected(bind_result.error());
+            hook.m_restorer = std::move(*restore_result);
+        }
+
+        if constexpr(Flag & StartDisabled) return hook;
+        if (auto enable_result = hook.enable(); !enable_result)
+            return std::unexpected(enable_result.error());
+
+        return hook;
+    }
+
+    static std::expected<InlineToMemberFunctionHook, IMFHError> create(void* target, void* instance, void* static_invoker,
+        const BindingThunk::ABISignature& target_signature, Flags flags = Default);
+
     InlineToMemberFunctionHook(InlineToMemberFunctionHook&& Other) noexcept = default;
     InlineToMemberFunctionHook& operator=(InlineToMemberFunctionHook&& Other) noexcept = default;
     InlineToMemberFunctionHook(const InlineToMemberFunctionHook& Other) = delete;
@@ -66,8 +115,26 @@ public:
     /// @brief Check if the hook is enabled.
     [[nodiscard]] bool enabled() const;
 
+    template <typename RetT = void, typename... Args> RetT unsafe_call(Args... args) {
+        return m_restorer ? static_cast<RetT (*)(Args...)>(m_restorer.get())(args...) : m_hook.unsafe_call<RetT, Args...>(args...);
+    }
+
+    template <typename RetT = void, typename... Args> RetT unsafe_call_original(Args...args) {
+        return m_hook.unsafe_call<RetT, Args...>(args...);
+    }
+
+    template <typename RetT = void, typename... Args> RetT unsafe_call_restore(Args... args) {
+        return static_cast<RetT (*)(Args...)>(m_restorer.get())(args...);
+    }
+
 private:
+    InlineToMemberFunctionHook() = default;
+
+    std::optional<IMFHError> setup(void* target, void* instance, void* static_invoker,
+        const BindingThunk::ABISignature& signature, Flags flags = Default);
+
     InlineHook m_hook{};
     BindingThunk::FThunkPtr m_binder{};
+    BindingThunk::FThunkPtr m_restorer{};
 };
 } // namespace safetyhook
--- src/imf_hook.cpp
+++ src/imf_hook.cpp
@@ -1,1 +1,46 @@
 #include "safetyhook/imf_hook.hpp"
+
+std::expected<safetyhook::InlineToMemberFunctionHook, safetyhook::InlineToMemberFunctionHook::IMFHError>
+safetyhook::InlineToMemberFunctionHook::create(void* target, void* instance, void* static_invoker,
+    const BindingThunk::ABISignature& target_signature, const Flags flags) {
+
+    InlineToMemberFunctionHook hook{};
+    if (const auto setup_result = hook.setup(target, instance, static_invoker, target_signature, flags))
+        return std::unexpected(*setup_result);
+    return hook;
+}
+std::expected<void, safetyhook::InlineHook::Error> safetyhook::InlineToMemberFunctionHook::enable() {
+    return m_hook.enable();
+}
+std::expected<void, safetyhook::InlineHook::Error> safetyhook::InlineToMemberFunctionHook::disable() {
+    return m_hook.disable();
+}
+bool safetyhook::InlineToMemberFunctionHook::enabled() const {
+    return m_hook.enabled();
+}
+
+std::optional<safetyhook::InlineToMemberFunctionHook::IMFHError> safetyhook::InlineToMemberFunctionHook::setup(
+    void* target, void* instance, void* static_invoker, const BindingThunk::ABISignature& signature,
+    const Flags flags) {
+
+    BindingThunk::EBindingThunkType thunk_flags = flags & PackArgs ? BindingThunk::EBindingThunkType::Argument : BindingThunk::EBindingThunkType::Default;
+    if (flags & SaveContext) thunk_flags |= BindingThunk::EBindingThunkType::Register;
+    BindingThunk::FThunkResult binding_thunk = BindingThunk::GenerateBindingThunk(static_invoker, instance, signature, thunk_flags);
+    if (!binding_thunk) return binding_thunk.error();
+    m_binder = std::move(*binding_thunk);
+
+    auto hook_result = InlineHook::create(target, binding_thunk->get(), InlineHook::StartDisabled);
+    if (!hook_result) return hook_result.error();
+    m_hook = std::move(*hook_result);
+
+    if (flags & PackArgs || flags & SaveContext) {
+        BindingThunk::FThunkResult restore_thunk = BindingThunk::GenerateRestoreThunk(m_hook.original<void*>(), signature, thunk_flags);
+        if (!restore_thunk) return restore_thunk.error();\
+        m_restorer = std::move(*restore_thunk);
+    }
+
+    if (flags & StartDisabled) return std::nullopt;
+    if (auto enable_result = m_hook.enable(); !enable_result)
+        return enable_result.error();
+    return std::nullopt;
+}
